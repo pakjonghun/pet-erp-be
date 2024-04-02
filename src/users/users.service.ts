c3968b64ddcd25e70e5da0a41e4c493a08e79968
fireking5997@gmail.com <fireking5997@gmail.com>
2024-04-02 11:51:01 +0900

    비밀번호 해쉬 기능 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
-  create(createUserInput: CreateUserInput) {
+  async create(createUserInput: CreateUserInput) {
+    const password = createUserInput.password;
+    const hashedPassword = await this.hashPassword(password);
+    createUserInput.password = hashedPassword;
     return this.userRepository.create(createUserInput);
   }
 
@@ -18,11 +22,23 @@ export class UsersService {
     return this.userRepository.findOne({ _id });
   }
 
-  update({ _id, ...body }: UpdateUserInput) {
+  async update(updateInput: UpdateUserInput) {
+    const password = updateInput.password;
+    if (updateInput.password) {
+      const hashedPassword = await this.hashPassword(password);
+      updateInput.password = hashedPassword;
+    }
+
+    const { _id, ...body } = updateInput;
     return this.userRepository.update({ _id }, body);
   }
 
   remove(id: string) {
     return this.userRepository.remove({ _id: id });
+  }
+
+  private async hashPassword(password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
   }
 }
