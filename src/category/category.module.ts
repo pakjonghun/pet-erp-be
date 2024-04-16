e3c30cb06ccd415b954711891d010c33091ceb3c
fireking5997@gmail.com <fireking5997@gmail.com>
2024-04-16 15:31:08 +0900

    카테고리 모듈 디비 임포트 및 모듈 임포트 작업

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
+import { DatabaseModule } from 'src/common/database/database.module';
+import { Category, categorySchema } from './entities/category.entity';
+import { CategoryRepository } from './category.repository';
 
 @Module({
-  providers: [CategoryResolver, CategoryService],
+  imports: [
+    // forwardRef(() => AppModule),
+    DatabaseModule.forFeature([
+      { name: Category.name, schema: categorySchema },
+    ]),
+  ],
+  providers: [CategoryResolver, CategoryService, CategoryRepository],
 })
 export class CategoryModule {}
