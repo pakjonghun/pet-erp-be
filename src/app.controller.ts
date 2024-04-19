f4d46868a0a5ce2c247fff01ac2471d28232ebe2
jonghunpak <fireking5997@gmail.co.kr>
2024-04-20 03:23:10 +0900

    다운로드 파일 이름은 client 에서 날짜와 서비스 조합으로 정함

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -60,7 +60,7 @@ export class AppController {
     await this.fileService.upload(file, service);
   }
 
-  @Get('/download/:service')
+  @Post('/download/:service')
   async download(@Param('service') service: string) {
     return this.fileService.download(service);
   }
--- src/common/interceptors/file.interceptor.ts
+++ src/common/interceptors/file.interceptor.ts
@@ -23,13 +23,15 @@ export class FileInspector implements NestInterceptor {
             'Content-Type',
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
           );
-          res.setHeader(
-            'Content-Disposition',
-            `attachment; filename=${this.utilService.getNowDate()}.xlsx`,
-          );
+          // res.setHeader(
+          //   'Content-Disposition',
+          //   `attachment; filename=${this.utilService.getNowDate()}.xlsx`,
+          // );
 
           return new StreamableFile(data);
         }
+
+        return data;
       }),
     );
   }
