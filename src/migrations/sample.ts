fe1c3e9ab957a8b2d1bc5b80f3f51eed36b8eea9
fireking5997@gmail.com <fireking5997@gmail.com>
2024-06-19 14:36:22 +0900

    모든 유저의 role 을 배열로 변경 마이그레이션 코드 추가

--- src/migrations/sample.ts
+++
@@ -1,7 +0,0 @@
-import { Db } from 'mongodb';
-
-module.exports = {
-  async up(db: Db) {
-    return db.collection('users').createIndex({ id: 1 }, { unique: true });
-  },
-};
---
+++ src/migrations/userRole.ts
@@ -0,0 +1,31 @@
+import { Db } from 'mongodb';
+
+module.exports = {
+  async up(db: Db) {
+    return db.collection('users').updateMany(
+      {},
+      {
+        $set: {
+          role: [
+            'BACK_DELETE',
+            'BACK_EDIT',
+            'STOCK_IN',
+            'STOCK_OUT',
+            'ORDER_CREATE',
+            'ORDER_EDIT',
+            'ORDER_DELETE',
+            'SALE_CREATE',
+            'SALE_EDIT',
+            'SALE_DELETE',
+            'ADMIN_ACCESS',
+            'ADMIN_IP',
+            'ADMIN_ACCOUNT',
+            'ADMIN_DELIVERY',
+            'ADMIN_LOG',
+          ],
+        },
+      },
+    );
+  },
+};
+//
